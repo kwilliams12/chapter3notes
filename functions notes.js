f36525b9a4f74e101c0517d39cf0b0402cd65505
d5bf <d5bf@fresnou.org>
2023-03-14 10:09:31 -0700

    initial commit

---
+++ functions notes.js
@@ -0,0 +1,79 @@
+//Functions Notes
+
+//1. define the function
+
+// function sayHello(){
+
+// console.log('Hello');
+
+// }
+
+//2. call or use the function
+
+// sayHello();
+
+// sayHello();
+
+// sayHello();
+
+// sayHello();
+
+// sayHello();
+
+//define
+
+// function printMax(){
+
+// let x = 6;
+
+// let y = 29;
+
+// if (x>y){
+
+// console.log(`the larger number is ${x}`);
+
+// } else {
+
+// console.log(`the larger number is ${y}`);
+
+// }
+
+// }
+
+//call
+
+// printMax();
+
+//define
+
+// function printMax(x, y){
+
+// if (x>y){
+
+// console.log(`the larger number is ${x}`);
+
+// } else {
+
+// console.log(`the larger number is ${y}`);
+
+// }
+
+// }
+
+//call
+
+// printMax(16, 9);
+
+// printMax(2, 75);
+
+// printMax(2, 2);
+
+function sum(num1, num2){
+
+    console.log(`${num1}+${num2}=${num1+num2}`);
+
+}
+
+sum(7, 10);
+
+sum(27, 1);
