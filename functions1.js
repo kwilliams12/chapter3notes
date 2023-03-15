24b14b3ddef9f40cf34e885f44750a616bbbf856
d5bf <d5bf@fresnou.org>
2023-03-15 10:29:38 -0700

    initial commit

---
+++ functions1.js
@@ -0,0 +1,40 @@
+// function sum(height,length){
+//
+//     console.log(`${height}*${length}=${height*length}`);
+//
+// }
+//
+// sum(7, 10);
+//
+// sum(27, 1);
+
+// function sum(radius,y){
+//
+//     console.log(`${radius}*${radius}*${y} =${radius * radius * y}`);
+//
+// }
+//
+// sum(3, 3.14);
+
+
+
+// function sum(num1, num2) {
+//
+//     console.log(`${num1}/${num2}=${num1 / num2}`);
+//
+//     if (num1 < num2); {
+//     }
+// }
+// sum(10, 2);
+//
+// sum(2, 4);\
+
+function sum(num1, num2){
+
+    console.log(`${num1}/${num2}=${num1/num2}`);
+
+}
+
+sum(10, 2);
+
+sum(2, 4);
--- functionsnotes.js
+++ functionsnotes.js
@@ -70,10 +70,10 @@
 
 function sum(num1, num2){
 
-    console.log(`${num1}+${num2}=${num1+num2}`);
+    console.log(`${num1}/${num2}=${num1/num2}`);
 
 }
 
-sum(7, 10);
+sum(10, 2);
 
-sum(27, 1);
+sum(2, 4);
