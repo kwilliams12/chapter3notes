de281ba735ed7a93c4e5fdeeb71f010a7e64c8b8
d5bf <d5bf@fresnou.org>
2023-03-23 10:29:51 -0700

    initial commit

---
+++ arraynotes1.js
@@ -0,0 +1,25 @@
+// function firstelement(arr){
+// console.log(arr[3])
+// }
+// let mynums=[3,4,5,6,]
+// firstelement(mynums)
+
+//
+// function firstelement(arr){
+//     console.log(arr[6])
+// }
+// let mynums=[3,4,5,6,]
+// firstelement(mynums)
+
+
+let students = ['"Broke array"'];
+
+let values = [1,];
+
+//accessing items in array
+
+console.log(students);
+
+console.log(students[0]); //beginning item has index or position 0
+
+console.log(students[1]); //index 1 means 1 item away from the beginning, or 2nd
---
+++ return1.js
@@ -0,0 +1,29 @@
+//
+// function printYes(){
+//
+//     console.log('Keion Williams');
+//
+// }
+//
+// printYes();
+//
+// printYes();
+//
+//
+//
+// function printText(text){
+//
+//     console.log(text);
+//
+// }
+//
+// printText('Keion Williams');
+//
+// printText('Williams Keion');
+//
+//
+// function double(num){
+//
+//     console.log(num*2);
+//
+// }
