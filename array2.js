703416c8f4017f6525189b4b26271f870f91e42c
d5bf <d5bf@fresnou.org>
2023-03-29 10:28:47 -0700

    initial commit

--- array2.js
+++ array2.js
@@ -1,15 +1,54 @@
-let vals = [1, 2, 3, 4, 5]
+// function countup(n) {
+//     if (n <= 0) {
+//         console.log('invalid');
+//     } else {
+//         let newarray = []
+//         for (let i = 1; i <= n; i++) {
+//             newarray.push(i)
+//         }
+//         console.log(newarray);
+//     }
+// }
+//
+// countup(16)
+// function countup(n) {
+//     if (n <= 0) {
+//         console.log('invalid');
+//     } else {
+//         let newarray = []
+//         for (let i = 1; i <= n; i++) {
+//             newarray.push(i)
+//         }
+//         console.log(newarray);
+//     }
+// }
+// //
+// countup(-16)
+// function countup(x) {
+//     if (x <= 0) {
+//         console.log('invalid');
+//     } else {
+//         let newarray = []
+//         for (let i = 1; i <= x; i++) {
+//             newarray.push(x)
+//         }
+//         console.log(newarray);
+//     }
+// }
+//
+// countup(44)
+// function countup(x) {
+//     if (x <= 4) {
+//         console.log('invalid');
+//     } else {
+//         let newarray = []
+//         for (let i = 2; i <= x; i++)
+//         for  (let i= x; i <= x; i++) {
+//             newarray.push(x)
+//         }
+//         console.log(newarray);
+//     }
+// }
+//
+// countup(23)
 
-let newVals = []
-
-for (let i=2; i<vals.length; i++){
-
-    newVals.push(vals[i]);
-
-}
-
-newVals[0]=7
-
-console.log('vals: ' + vals)
-
-console.log('newVals: ' + newVals)
--- arraynotes2.js
+++ arraynotes2.js
@@ -14,15 +14,15 @@
 
 //was equal to it
 
-let nums = [5, 4, 3, 2, 1]
-
-let newNums = nums
-
-newNums[0] = 12
-
-console.log('nums: ' + nums)
-
-console.log('newNums: ' + newNums)
+// let nums = [5, 4, 3, 2, 1]
+//
+// let newNums = nums
+//
+// newNums[0] = 12
+//
+// console.log('nums: ' + nums)
+//
+// console.log('newNums: ' + newNums)
 
 //notice both changed?
 
