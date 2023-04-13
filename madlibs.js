7021ccbd184d7783a5232afd73af45555effb6f6
d5bf <d5bf@fresnou.org>
2023-04-13 10:28:24 -0700

    initial commit

--- madlibs.js
+++ madlibs.js
@@ -1,4 +1,5 @@
 const readLine=require('readline-sync')
+const readline = require("readline");
 readLine.question('enter a noun');
 readLine.question('enter a verb');
 readLine.question('enter a plural noun');
@@ -18,3 +19,10 @@ readLine.question('enter a noun');
 readLine.question('enter a verb');
 readLine.question('enter a plural noun');
 readLine.question('enter a noun');
+
+let mylist=[];
+mylist.pushreadline.question('enter a adjective:');
+mylist.pushreadline.question('enter a plural noun:');
+mylist.pushreadline.question('enter a verb:');
+mylist.pushreadline.question('enter a noun:');
+console.log('my list has ${my-list[,]}and.${my-list[0]}')
---
+++ magic8ball.js
@@ -0,0 +1,5 @@
+let Answerlist = ['yes' , 'no' , 'maybe'];
+
+let randomNum = Math.floor(Math.random()*Answerlist.length);
+let randomAnswer = Answerlist[randomNum];
+console.log(randomAnswer);
