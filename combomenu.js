e6f343214c94f40c514bb97782193de9b714619c
d5bf <d5bf@fresnou.org>
2023-05-04 10:30:07 -0700

    initial commit

--- combomenu.js
+++ combomenu.js
@@ -2,11 +2,11 @@ const readLine=require('readline-sync')
 console.log(randomAnswer);
 
 let sandwhich=readLine.question('heres the type of sandwhichs we have tofu,beef,chicken which one would you like');
-console.log(sandwhich); console.log(drink); console.log(fries);
+console.log(sandwhich); console.log(drink); console.log(fries); console.log(cost); console.log(sauce);
 let sandwhich=readLine.question('enter a sandwhich');
-let drink=readLine.question('would you like a drink with that?');
-let drink=readLine.question('what size?');
-let fries=readLine.question('would you like french fries');
-let drink=readLine.question('what size?');
-let drink=readLine.question('what size?');
-
+let drink choose=readLine.question('would you like a drink with that?');
+let drink size=readLine.question('what size?');
+let fries =readLine.question('would you like french fries?');
+let fries size=readLine.question('what size?');
+let sauce=readLine.question('would you also like sauce?');
+let cost=readLine.question('your total cost is 8.70');
